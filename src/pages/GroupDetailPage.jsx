65cef4fc54d6f1a7c6c395f70fec6108e7669a35
justin <justinliehu@gmail.com>
2026-04-17 09:31:07 +0200

    build: rebuild with wallet auto-reconnect + delete sync

--- src/pages/GroupDetailPage.jsx
+++ src/pages/GroupDetailPage.jsx
@@ -342,30 +342,3 @@ export default function GroupDetailPage() {
     </div>
   );
 }
-/}
-                      <button
-                        className="btn btn-ghost btn-xs btn-circle opacity-30 hover:opacity-100"
-                        onClick={(e) => { e.stopPropagation(); handleDeleteExpense(exp.id); }}
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                      </button>
-                    </motion.div>
-                  );
-                })}
-              </div>
-            </div>
-          ))
-        )
-      )}
-
-      {showInvite && <QRInvite group={group} onClose={() => setShowInvite(false)} />}
-    </div>
-  );
-}
-
-    </div>
-  );
-}
-}
